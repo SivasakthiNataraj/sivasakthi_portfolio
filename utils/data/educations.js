92f3a2366943e6b8505331a9fbf988c28e8a22d1
Sivasakthi_Nataraj <sivasakthinataraj@gmail.com>
2025-11-30 00:29:45 +0530

    Initial Commit

---
+++ utils/data/educations.js
@@ -0,0 +1,8 @@
+export const educations = [
+  {
+    id: 1,
+    title: "Bachelor Degree",
+    duration: "May 2013 - May 2017",
+    institution: "Thiagarajar College of Engineering",
+  }
+]
---
+++ utils/data/experience.js
@@ -0,0 +1,26 @@
+export const experiences = [
+  {
+    id: 1,
+    title: 'Engineer - XR Developer',
+    company: "Renault Nissan Technology & Business Centre India Private Limited",
+    duration: "(Apr 2023 - Present)"
+  },
+  {
+    id: 2,
+    title: "Associate Software Developer - XR",
+    company: "Novac Technology Solutions",
+    duration: "(Apr 2022 – Apr 2023)"
+  },
+  {
+    id: 3,
+    title: "Associate Graphic Designer",
+    company: "LMES Academy",
+    duration: "(Jul 2021 – Apr 2022)"
+  },
+  {
+    id: 4,
+    title: "Game Developer - XR",
+    company: "Kewaunee International Group",
+    duration: "(Jan 2019 – Feb 2020)"
+  }
+]
